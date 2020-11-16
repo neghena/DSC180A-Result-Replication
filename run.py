bab8fd38daef8c800fee4b520e1c648b637206f4
neghena <nfaizyar@ucsd.edu>
2020-11-15 19:27:47 -0800

    fix targets

--- run.py
+++ run.py
@@ -7,10 +7,11 @@ import json
 #sys.path.insert(0, 'src/analysis')
 #sys.path.insert(0, 'src/model')
 
-from data/etl import get_data
-from analysis/analysis import compute_aggregates
-from model/model import train
+#from data/etl import get_data
+#from analysis/analysis import compute_aggregates
+#from model/model import train
 from clean_gps import clean_gps
+from robot import run_robot
 
 
 def main(targets):
@@ -21,27 +22,6 @@ def main(targets):
     `main` runs the targets in order of x=>y=>z.
     '''
 
-    if 'data' in targets:
-        with open('config/data-params.json') as fh:
-            data_cfg = json.load(fh)
-
-        # make the data target
-        data = get_data(**data_cfg)
-
-    if 'analysis' in targets:
-        with open('config/analysis-params.json') as fh:
-            analysis_cfg = json.load(fh)
-
-        # make the data target
-        compute_aggregates(data, **analysis_cfg)
-
-    if 'model' in targets:
-        with open('config/model-params.json') as fh:
-            model_cfg = json.load(fh)
-
-        # make the data target
-        train(data, **model_cfg)
-
     if 'report' in targets:
         with open('notebooks/report.ipynb') as fh:
             report_cfg = json.load(fh) # how do you just open a jupyter notebook/turn it into a pdf?
@@ -51,6 +31,33 @@ def main(targets):
             clean_gps_cfg = json.load(fh)
 
         clean_gps(data, **clean_gps_cfg)
+
+    if 'run_robot' in targets:
+        with open('config/run_robot.json') as fh:
+            robot_cfg = json.load(fh)
+        run_robot(data, **robot_cfg)
+
+    #if 'data' in targets:
+        #with open('config/data-params.json') as fh:
+        #    data_cfg = json.load(fh)
+
+        # make the data target
+        #data = get_data(**data_cfg)
+
+    #if 'analysis' in targets:
+        #with open('config/analysis-params.json') as fh:
+            #analysis_cfg = json.load(fh)
+
+        # make the data target
+        #compute_aggregates(data, **analysis_cfg)
+
+    #if 'model' in targets:
+        #with open('config/model-params.json') as fh:
+            #model_cfg = json.load(fh)
+
+        # make the data target
+        #train(data, **model_cfg)
+
 
     return
 
