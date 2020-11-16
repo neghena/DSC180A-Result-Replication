ce2b33a66d4314ce833cfd633d008e222523b844
ssliang <43868758+ssliang@users.noreply.github.com>
2020-11-15 22:36:24 -0500

    Update analysis.py
    updated translate to cartesian function,

--- src/analysis/analysis.py
+++ src/analysis/analysis.py
@@ -4,25 +4,29 @@ from math import sin, cos, sqrt, atan2, radians
 def translate_coordinates(x):
     return
 
+def translate_to_cartesian(latitude, longitude):
+    earth_radius = 6371
+    x = earth_radius * cos(latitude) * cos(longitude)
+    y = earth_radius * cos(latitude) * sin(longitude)
+    return x, y
+
 def calculate_distance(x,y):
     """
     Calculates the distance between two gps coordinates
-    X should be a list of [lon1, lat1]
-    y should be a list of [lon2, lat2]
+    X should be a list of [long1, lat1]
+    y should be a list of [long2, lat2]
     """
     # return geopy.distance.vincenty(x, y).km
-
-    R = 6370 # rad of earth
-
+    R = 6370
     lat1 = radians(x[0])  #insert value
     lon1 = radians(x[1])
     lat2 = radians(y[0])
     lon2 = radians(y[1])
 
-    difflon = lon2 - lon1
-    difflat = lat2- lat1
+    dlon = lon2 - lon1
+    dlat = lat2- lat1
 
-    a = sin(difflat / 2)**2 + cos(lat1) * cos(lat2) * sin(difflon / 2)**2
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
     c = 2 * atan2(sqrt(a), sqrt(1-a))
     distance = R * c
     return distance
