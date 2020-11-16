419e3fc2a3224b0e7bc5d52079c9cfd0db6fdcfc
Garrett Gibo <garrett.gibo@gmail.com>
2020-11-15 19:54:16 -0800

    Test robot

--- src/robot.py
+++ src/robot.py
@@ -1,2 +1,17 @@
-def run_robot(x):
-    return "hello"
+from pymavlink import mavutil
+
+class Robot:
+    def __init__(self, url):
+        self.url = url
+        self.mavcar = mavutil.mavlink_connection(url)
+        self.closed = False
+
+    def get_gps(self):
+        while not self.closed:
+            coords = self.mavcar.location()
+            print(coords)
+
+if __name__ == "__main__":
+    url = "tcp:127.0.0.1:5760"
+    robot = Robot(url)
+    robot.get_gps()
